65f9647f030da98964002e04b953aa6bef0ead22
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-10-20 14:19:59 -0400

    working with subscriptions, clean-up, and unsubscribing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import { Observable } from 'rxjs';
+import { Observable, subscribeOn } from 'rxjs';
 
 const observable = new Observable((subscriber) => {
-  subscriber.next('Hello');
-  subscriber.next('World');
-  subscriber.complete();
+  const id = setInterval(() => {
+    subscriber.next('hi');
+    console.log('inside interval');
+  }, 1000);
+
+  return () => {
+    clearInterval(id);
+    console.log('clean up');
+  };
 });
 
-observable.subscribe({
+console.log('before');
+
+const subscription = observable.subscribe({
   next: (value) => console.log(value),
   error: (err) => console.error(err),
   complete: () => console.log('done'),
 });
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 3000);
+
+console.log('after');
